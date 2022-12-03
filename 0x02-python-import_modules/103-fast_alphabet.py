b7210a7bc0c9c6d316c18a8d3b22c3f57a3d7873
LizzyDbosstechgirl <aladeelizabeth91@gmail.com>
2022-12-02 22:05:46 -0800

    tasks 9

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    if number < 0:
-        number = -number
-    last = number % 10
-    print(last, end="")
-    return last
+from string import ascii_uppercase
+print(ascii_uppercase)
