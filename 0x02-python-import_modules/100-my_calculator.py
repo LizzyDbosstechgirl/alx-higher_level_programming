8045f82e2783f611cf64ec19f92e1df492d2e3e7
LizzyDbosstechgirl <aladeelizabeth91@gmail.com>
2022-12-02 21:59:14 -0800

    tasks 6py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
-for i in range(9):
-    for j in range(10):
-        if i < j:
-            if ((i*10) + j) != 89:
-                print("{}{}".format(i, j), end=", ")
-            else:
-                print("{}{}".format(i
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+ops = ['+', '-', '*', '/']
+argc = len(argv)
+
+if __name__ == '__main__':
+    if (argc - 1) < 3:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    elif argv[2] not in ops:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+    else:
+        a, b = int(argv[1]), int(argv[3])
+        if argv[2] == '+':
+            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+        elif argv[2] == '-':
+            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+        elif argv[2] == '*':
+            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+        else:
+            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
