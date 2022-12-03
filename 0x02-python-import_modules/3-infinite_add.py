3457e2fd567423342c0d064a4c475b3e57689872
LizzyDbosstechgirl <aladeelizabeth91@gmail.com>
2022-12-02 21:52:03 -0800

    tasks 3 py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if (chr(i) != 'q' and chr(i) != 'e'):
-        print('{}'.format(chr(i)), end="")
+from sys import argv
+i = 1
+length = len(argv)
+sum = 0
+if __name__ == '__main__':
+    for num in range(1, length):
+        sum += int(argv[num])
+print(sum)
 
