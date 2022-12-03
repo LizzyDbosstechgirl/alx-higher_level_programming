4507a56ebff83cc4e1c0628127097b4b6b810d5a
LizzyDbosstechgirl <aladeelizabeth91@gmail.com>
2022-12-02 21:53:48 -0800

    tasks 4py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print('{} = {}'.format(i, hex(i))
+if __name__ == '__main__':
+    import hidden_4
+    for item in dir(hidden_4):
+        if item[0:2] != "__":
+            print("{:s}".format(item))
