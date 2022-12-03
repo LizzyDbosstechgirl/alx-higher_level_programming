b5046e710d834cceadd48a0248255d233598c979
LizzyDbosstechgirl <aladeelizabeth91@gmail.com>
2022-12-02 22:26:49 -0800

    tasks 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if idx < 0 or idx > len(new_list)- 1:
+        return (new_list)
+    else:
+        new_list[idx] = element
+        return (new_list)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
