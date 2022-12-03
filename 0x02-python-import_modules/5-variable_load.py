f25ab258b42814e2c7ff343146bc3bfb21552b4b
LizzyDbosstechgirl <aladeelizabeth91@gmail.com>
2022-12-02 21:55:57 -0800

    tasks 5py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for num in range(0, 100):
-    if num != 99:
-        print("{:02d}, ".format(num), end='')
-    else:
-        print("{:02d}".format(num))
+from variable_load_5 import a
+
+if __name__ == '__main__':
+    print(a)
