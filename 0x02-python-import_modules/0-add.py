f187ce965f7142dd4b3688b4cfcd0b31955c88d9
LizzyDbosstechgirl <aladeelizabeth91@gmail.com>
2022-12-02 21:44:38 -0800

    tasks 0py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-if number > 0:
-    print('{} is positive'.format(number))
-elif(number == 0):
-    print('{} is zero'.format(number))
-else:
-    print('{} is negative'.format(number))
+from add_0 import add
+a = 1
+b = 2
+if __name__ == "__main__":
+    print('{} + {} = {}'.format(a, b, add(a, b)))
