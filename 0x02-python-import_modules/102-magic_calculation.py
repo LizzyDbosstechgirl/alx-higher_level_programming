2cf04a49f239916c314925ad0a260c485c1f17d0
LizzyDbosstechgirl <aladeelizabeth91@gmail.com>
2022-12-02 22:03:44 -0800

    tasks 8

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-def magic_calculation(a, b, c):
-    if (a < b):
-        return (c)
-    elif (c > b):
-        return (a + b)
+from magic_calculation_102 import add, sub
+
+
+def magic_calculation(a, b):
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
     else:
-        return (a * b - c)
+        return (sub(a, b))
