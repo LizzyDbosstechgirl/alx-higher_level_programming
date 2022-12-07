a01f3b22d21002e12f3431596265a4797c0e7430
LizzyDbosstechgirl <aladeelizabeth91@gmail.com>
2022-12-07 13:35:41 -0800

    tasks 8

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        a_dictionary.pop(key)
+        return a_dictionary
+    else:
+        return (a_dictionary)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
